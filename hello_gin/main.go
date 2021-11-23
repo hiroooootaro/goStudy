5c306017571a649a798a5dedf1ccd746fac8045b
hiroooootaro <taro.hirotaro@icloud.com>
2021-11-23 19:15:49 +0900

    docker設定

--- hello_gin/main.go
+++ hello_gin/main.go
@@ -1,18 +1,7 @@
 package main
-import (
-  "log"
-  "github.com/gin-gonic/gin"
-)
+
+import "fmt"
+
 func main() {
-  // ルーターを準備 --- (*1)
-  router := gin.Default()
-  // URIとハンドラを指定 --- (*2)
-  router.GET("/", func(ctx *gin.Context) {
-    ctx.String(200, "Hello, World!")
-  })
-  // サーバーを起動 --- (*3)
-  err := router.Run("127.0.0.1:8888")
-  if err != nil {
-    log.Fatal("サーバー起動に失敗", err)
-  }
+  fmt.Println("Hello, World!")
 }
